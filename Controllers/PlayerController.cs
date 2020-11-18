626aa7ac56a0b1f275ee3610faddbf9b17c4cf78
Matt Wrench <matt@mattwrench.com>
2020-11-18 14:23:21 -0600

    Add PlayerController class

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using Paddlist.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paddlist.Controllers
+{
+    class PlayerController : PaddleController
+    {
+        public PlayerController(World world) : base(world)
+        {
+        }
+
+        public override void Update(float dt)
+        {
+            setVelocity(world.Player);
+            setPosition(world.Player, dt);
+            world.Player.SetBounds();
+            boundsCheck(world.Player);
+        }
+
+        protected override void setVelocity(Paddle paddle)
+        {
+            // TODO
+        }
+    }
+}
