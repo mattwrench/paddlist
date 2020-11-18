b419afcc9767e44bb928700bce49c0b454bb2ea9
Matt Wrench <matt@mattwrench.com>
2020-11-18 14:35:42 -0600

    Add Input class

---
+++ Controllers/Input.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paddlist.Controllers
+{
+    static class Input
+    {
+        private static KeyboardState keyboardState, lastKeyboardState;
+        
+        public static void Update()
+        {
+            lastKeyboardState = keyboardState;
+            keyboardState = Keyboard.GetState();
+        }
+
+        public static bool MovingUp
+        {
+            get
+            {
+                return keyboardState.IsKeyDown(Keys.Up) || keyboardState.IsKeyDown(Keys.W);
+            }
+        }
+
+        public static bool MovingDown
+        {
+            get
+            {
+                return keyboardState.IsKeyDown(Keys.Down) || keyboardState.IsKeyDown(Keys.S);
+            }
+        }
+    }
+}
