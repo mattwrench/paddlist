c02521c78b892278942704b70d63281c5fa61743
Matt Wrench <matt@mattwrench.com>
2020-11-18 15:39:17 -0600

    Bind balls inside screen

--- Controllers/BallController.cs
+++ Controllers/BallController.cs
@@ -47,7 +47,38 @@ namespace Paddlist.Controllers
 
         protected override void boundsCheck(Entity entity)
         {
-            // TODO
+            // Left wall
+            if (entity.Bounds.X < 0)
+            {
+                entity.Bounds.X = 0;
+                entity.Position.X = entity.Bounds.X + entity.Bounds.Width / 2;
+                entity.Velocity.X *= -1;
+            }
+
+            // Right wall
+            else if (entity.Bounds.X + entity.Bounds.Width > world.Width)
+            {
+                entity.Bounds.X = world.Width - entity.Bounds.Width;
+                entity.Position.X = entity.Bounds.X + entity.Bounds.Width / 2;
+                entity.Velocity.X *= -1;
+            }
+
+            // Top wall
+            // Uses if, not else if, since ball can be out of bounds (left || right) && (top || bottom)
+            if (entity.Bounds.Y < 0)
+            {
+                entity.Bounds.Y = 0;
+                entity.Position.Y = entity.Bounds.Y + entity.Bounds.Height / 2;
+                entity.Velocity.Y *= -1;
+            }
+
+            // Bottom wall
+            else if (entity.Bounds.Y + entity.Bounds.Height > world.Height)
+            {
+                entity.Bounds.Y = world.Height - entity.Bounds.Height;
+                entity.Position.Y = entity.Bounds.Y + entity.Bounds.Height / 2;
+                entity.Velocity.Y *= -1;
+            }
         }
     }
 }
