398f7eeb958d4a9385dd9e79b460dc30a1248467
Matt Wrench <matt@mattwrench.com>
2020-11-18 14:14:27 -0600

    Add EntityController class

---
+++ Controllers/EntityController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Paddlist.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paddlist.Controllers
+{
+    // Base class for all controllers
+    abstract class EntityController
+    {
+        protected World world;
+
+        public EntityController(World world)
+        {
+            this.world = world;
+        }
+
+        public abstract void Update(float dt);
+        protected abstract void boundsCheck(Entity entity);
+
+        // Add velocity * dt to position
+        protected void setPosition(Entity entity, float dt)
+        {
+            entity.Position = Vector2.Add(entity.Position, Vector2.Multiply(entity.Velocity, dt));
+        }
+    }
+}
