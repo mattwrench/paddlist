0c7bf4298d89c652e2eb2c22f9edeaf05ad8762a
Matt Wrench <matt@mattwrench.com>
2020-11-18 14:18:46 -0600

    Add PaddleController class

---
+++ Controllers/PaddleController.cs
@@ -0,0 +1,21 @@
+﻿using Paddlist.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paddlist.Controllers
+{
+    abstract class PaddleController : EntityController
+    {
+        public PaddleController(World world) : base(world)
+        {
+        }
+
+        protected abstract void setVelocity(Paddle paddle);
+
+        protected override void boundsCheck(Entity entity)
+        {
+            // TODO
+        }
+    }
+}
