32b9a4c0abb5355828452b7d149584e98f8d9f91
Matt Wrench <matt@mattwrench.com>
2020-11-18 21:29:28 -0600

    Add enemy movement AI

--- Controllers/EnemyController.cs
+++ Controllers/EnemyController.cs
@@ -1,4 +1,5 @@
-﻿using Paddlist.Models;
+﻿using Microsoft.Xna.Framework;
+using Paddlist.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +8,8 @@ namespace Paddlist.Controllers
 {
     class EnemyController : PaddleController
     {
+        private const int SlowDownRange = 30;
+
         public EnemyController(World world) : base(world)
         {
         }
@@ -22,7 +25,43 @@ namespace Paddlist.Controllers
         // Enemy AI
         protected override void setVelocity(Paddle paddle)
         {
-            // TODO
+            // Find nearest ball
+            Ball nearestBall = null;
+            foreach (Ball ball in world.Balls)
+            {
+                if (ball.Velocity.X > 0)
+                {
+                    // Do not compare pos.X if first ball
+                    if (nearestBall == null)
+                    {
+                        nearestBall = ball;
+                        continue;
+                    }
+
+                    // Save ball with the furthest X value
+                    if (ball.Position.X > nearestBall.Position.X)
+                        nearestBall = ball;
+                }
+            }
+
+            // Move towards nearest ball
+            paddle.Velocity = new Vector2();
+            if (nearestBall != null)
+            {
+                // Ball is above paddle
+                if (nearestBall.Position.Y < paddle.Position.Y)
+                    paddle.Velocity.Y -= paddle.TopSpeed;
+                // Ball is below paddle
+                else if (nearestBall.Position.Y > paddle.Position.Y)
+                    paddle.Velocity.Y += paddle.TopSpeed;
+
+                // Slow down if ball and paddle are vertically close together
+                float dist = Math.Abs(paddle.Position.Y - nearestBall.Position.Y);
+                if (dist < SlowDownRange)
+                {
+                    paddle.Velocity.Y *= (dist / SlowDownRange);
+                }
+            }
         }
     }
 }
