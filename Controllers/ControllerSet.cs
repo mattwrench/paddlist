5c785855910195191f78379ee281bde3e7aab99f
Matt Wrench <matt@mattwrench.com>
2020-11-18 21:21:36 -0600

    Add EnemyController class

--- Controllers/ControllerSet.cs
+++ Controllers/ControllerSet.cs
@@ -8,17 +8,20 @@ namespace Paddlist.Controllers
     class ControllerSet
     {
         private PlayerController playerController;
+        private EnemyController enemyController;
         private BallController ballController;
 
         public ControllerSet(World world)
         {
             playerController = new PlayerController(world);
+            enemyController = new EnemyController(world);
             ballController = new BallController(world);
         }
 
         public void Update(float dt)
         {
             playerController.Update(dt);
+            enemyController.Update(dt);
             ballController.Update(dt);
         }
     }
---
+++ Controllers/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using Paddlist.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paddlist.Controllers
+{
+    class EnemyController : PaddleController
+    {
+        public EnemyController(World world) : base(world)
+        {
+        }
+
+        public override void Update(float dt)
+        {
+            setVelocity(world.Enemy);
+            setPosition(world.Enemy, dt);
+            world.Enemy.SetBounds();
+            boundsCheck(world.Enemy);
+        }
+
+        // Enemy AI
+        protected override void setVelocity(Paddle paddle)
+        {
+            // TODO
+        }
+    }
+}
